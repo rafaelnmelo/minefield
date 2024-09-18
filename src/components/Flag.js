8aaf84099a70430dabc41fd83929bdf089deac90
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-09-18 15:05:52 -0300

    Ajustes componente Flag

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,68 +1,68 @@
-import React from "react"
-import { SafeAreaView, StyleSheet } from "react-native"
+import React from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
 
 export default props => {
     return (
-        <SafeAreaView style={styles.container} >
-            <SafeAreaView style={[styles.flagPole, props.bigger ? styles.biggerFlagPole : null]} />
-            <SafeAreaView style={[styles.flag, props.bigger ? styles.biggerFlag : null]} />
-            <SafeAreaView style={[styles.base1, props.bigger ? styles.base1 : null]} />
-            <SafeAreaView style={[styles.base2, props.bigger ? styles.base2 : null]} />
+        <SafeAreaView style={styles.container}>
+           <SafeAreaView style={[styles.flagpole, props.bigger ? styles.flagpoleBigger : null]} />
+           <SafeAreaView style={[styles.flag, props.bigger ? styles.flagBigger : null]} />
+           <SafeAreaView style={[styles.base1, props.bigger ? styles.base1Bigger : null]} />
+           <SafeAreaView style={[styles.base2, props.bigger ? styles.base2Bigger : null]} /> 
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 2
+        marginTop: 2,
     },
-    flagPole: {
+    flagpole: {
         position: 'absolute',
         height: 14,
         width: 2,
+        backgroundColor: '#222',
         marginLeft: 9,
-        backgroundColor: '#222'
     },
     flag: {
         position: 'absolute',
         height: 5,
         width: 6,
+        backgroundColor: '#F22',
         marginLeft: 3,
-        backgroundColor: '#F22'
     },
     base1: {
         position: 'absolute',
         height: 2,
         width: 6,
-        marginLeft: 7,
         backgroundColor: '#222',
+        marginLeft: 7,
         marginTop: 10,
     },
     base2: {
         position: 'absolute',
         height: 2,
         width: 10,
-        marginLeft: 5,
         backgroundColor: '#222',
-        marginTop: 12
+        marginLeft: 5,
+        marginTop: 12,
     },
-    biggerFlagPole: {
+    flagpoleBigger: {
         height: 28,
         width: 4,
         marginLeft: 16,
     },
-    biggerFlag: {
+    flagBigger: {
         height: 10,
         width: 14,
         marginLeft: 3,
     },
-    biggerBase1: {
+    base1Bigger: {
         height: 4,
         width: 12,
-        marginLeft: 12,
         marginTop: 20,
+        marginLeft: 12,
     },
-    biggerBase2: {
+    base2Bigger: {
         height: 4,
         width: 20,
         marginLeft: 8,
